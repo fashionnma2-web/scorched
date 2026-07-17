8ae705a0201d1db2fb9778b2a99df17b1f751fd4
fashionnma2-web <fashionnma2@gmail.com>
2026-07-17 16:40:35 +0100

    Update agentscan_indexer.go

--- indexer/agentscan_indexer.go
+++ indexer/agentscan_indexer.go
@@ -504,9 +504,10 @@ func NewIndexer(cfg *Config) (*Indexer, error) {
 
 	// Connect to Redis
 	redisClient := redis.NewClient(&redis.Options{
-		Addr:     cfg.RedisAddr,
-		Password: cfg.RedisPassword,
-	})
+    Addr:      cfg.RedisAddr,
+    Password:  cfg.RedisPassword,
+    TLSConfig: &tls.Config{},
+})
 	if err := redisClient.Ping(ctx).Err(); err != nil {
 		return nil, fmt.Errorf("ping redis: %w", err)
 	}
